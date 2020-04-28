9c8bd99d06db95d1dc75076fa8a07ca02a666cb6
Ed <edmundocall.eoc@gmail.com>
2020-04-28 22:36:29 +0000

    added div containers, rows and cols for uers orders, also added if statement in settings.py to check whether to use test DB or not production

--- honda_ecommerce/settings.py
+++ honda_ecommerce/settings.py
@@ -91,16 +91,18 @@ WSGI_APPLICATION = 'honda_ecommerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+if os.path.exists('env.py'):
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
 
-# DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
-
-
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL')) }
 
 
 # Password validation
