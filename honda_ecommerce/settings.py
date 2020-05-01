fe5ea62387309152f2aa7a21ef2731be53c79404
Ed <edmundocall.eoc@gmail.com>
2020-05-01 19:14:39 +0000

    added gitignore satic and commented out env.py

--- honda_ecommerce/settings.py
+++ honda_ecommerce/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 #----------------- import env file if path exists, import database -
 import os
 
-if os.path.exists('env.py'):
-    import env
+# if os.path.exists('env.py'):
+#     import env
 
 import dj_database_url
 
